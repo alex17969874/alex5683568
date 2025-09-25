354d9cf70e5c1023d6d1df3cb8f2eb2a4833a0a5
alex17969874 <aleks2007aleks10@gmail.com>
2025-09-25 11:17:41 +0300

    First commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        float num = float.Parse(input);
+        if (num / 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+    }
+}
