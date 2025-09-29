b8d83489b9e2968bf5dbc7f86dace30acdd7c171
alahuakbhar <et.vasil.i@eduiteh.eu>
2025-09-29 10:58:30 +0300

    modna i klass

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualBasic;
+
+string input = Console.ReadLine();
+int year = int.Parse(input);
+
+if (((year % 4) == 0) && ((year % 100) != 0))
+    Console.WriteLine("leap year");
+
+if (year % 400 == 0)
+    Console.WriteLine("leap year");
