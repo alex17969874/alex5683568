9fd5439881d5cafd8e54864371e605b3cc0bdf68
IDK <>
2025-10-16 11:06:25 +0300

    domashno

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("name:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("age:");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("total price:");
+        double totalAmount = double.Parse(Console.ReadLine());
+
+        double discount = 0;
+
+        if (totalAmount < 50)
+        {
+            discount = 0;
+        }
+        else if (totalAmount >= 50 && totalAmount <= 100)
+        {
+            discount = 0.05;
+        }
+        else
+        {
+            discount = 0.10; 
+        }
+
+        if (age < 18)
+        {
+            discount += 0.02; 
+        }
+
+        double discountAmount = totalAmount * discount;
+        double finalAmount = totalAmount - discountAmount;
+
+        Console.WriteLine($"{name}");
+        Console.WriteLine($"{finalAmount:F2}"); 
+    }
+}
